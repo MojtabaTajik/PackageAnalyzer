46fc33f5fc1913956d4a51045af783b0c99a69fe
Mojtaba Tajik <onyfel@gmail.com>
2024-09-15 09:21:56 +0200

    Add PackageConfigParser.cs

---
+++ PackageAnalyzer/Services/PackageConfigParser.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+using PackageAnalyzer.Models;
+
+namespace PackageAnalyzer.Services;
+
+public class PackageConfigParser
+{
+    public List<PackageInfo> GetPackages(string configContent)
+    {
+        if (string.IsNullOrEmpty(configContent))
+        {
+            return [];
+        }
+        
+        XDocument doc = XDocument.Parse(configContent);
+
+        var packages = doc.Descendants("package")
+            .Select(package => new PackageInfo(
+                GetAttributeValue(package, "id"),
+                GetAttributeValue(package, "version"),
+                GetAttributeValue(package, "targetFramework")))
+            .ToList();
+
+        return packages;
+    }
+    
+    private string GetAttributeValue(XElement element, string attributeName)
+    {
+        return element.Attribute(attributeName)?.Value ?? string.Empty;
+    }
+}
